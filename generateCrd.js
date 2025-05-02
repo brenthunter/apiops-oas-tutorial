363391e60d73c3f7d518a7b4e63bbb8216b7f0b2
Brent Hunter <brenthunter@users.noreply.github.com>
2025-05-02 09:10:45 +0100

    Update generateCrd.js

--- generateCrd.js
+++ generateCrd.js
@@ -35,9 +35,9 @@ function generateCRD(openApiData, propertiesData) {
           enabled: true,
           selectors: [
             {
-              type: "HTTP",
-              path: "/",
-              pathOperator: "EQUALS",
+              type: "\"HTTP\"",
+              path: "\"/\"",
+              pathOperator: "\"EQUALS\"",
               methods: [ ],
             },
           ],
@@ -45,7 +45,7 @@ function generateCRD(openApiData, propertiesData) {
             name: "Rate Limit",
             description: "Rate limiting",
             enabled: true,
-            policy: "rate-limit",
+            policy: "\"rate-limit\"",
             configuration: {
               async: false,
               addHeaders: true,
@@ -53,7 +53,7 @@ function generateCRD(openApiData, propertiesData) {
                 useKeyOnly: false,
                 periodTime: 1,
                 limit: 5,
-                periodTimeUnit: "MINUTES",
+                periodTimeUnit: "\"MINUTES\"",
               },
             },
           } ],
