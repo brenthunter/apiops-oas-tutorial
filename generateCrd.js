5a448a7d8b4b9a9d31b2c4af94b020aa901023bc
Brent Hunter <brenthunter@users.noreply.github.com>
2025-05-06 16:53:45 +0100

    Update generateCrd.js

--- generateCrd.js
+++ generateCrd.js
@@ -99,7 +99,10 @@ function generateCRD(openApiData, propertiesData) {
         enabled: true,
     }]
     : [ ];
-  resources[0].configuration.content = openApiData;
+  resources[0].configuration.content = openApiData.toString();
+  resources[0].configuration.content2 = JSON.stringify(openApiData);
+  resources[0].configuration.content3 = JSON.stringify(openApiData.toString());
+  resources[0].configuration.content4 = JSON.stringify(openApiData).toString();
   
   const crd = {
     apiVersion: "gravitee.io/v1alpha1",
