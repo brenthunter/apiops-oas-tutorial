beed46122c45febb7f5106c865cdc790128909d0
Brent Hunter <brenthunter@users.noreply.github.com>
2025-05-06 13:33:34 +0100

    Update generateCrd.js

--- generateCrd.js
+++ generateCrd.js
@@ -90,13 +90,13 @@ function generateCRD(openApiData, propertiesData) {
 
   var myJSONString = JSON.stringify(openApiData);
   var strOpenApiData = myJSONString.replace(/[\\]/g, '\\\\')
-    .replace(/[\"]/g, '\\\"')
-    .replace(/[\/]/g, '\\/')
-    .replace(/[\b]/g, '\\b')
-    .replace(/[\f]/g, '\\f')
-    .replace(/[\n]/g, '\\n')
-    .replace(/[\r]/g, '\\r')
-    .replace(/[\t]/g, '\\t');
+                                   .replace(/[\"]/g, '')
+                                   .replace(/[\/]/g, '\\/')
+                                   .replace(/[{]/g, '')
+                                   .replace(/[}]/g, '')
+                                   .replace(/[\n]/g, '\\n')
+                                   .replace(/[\r]/g, '\\r')
+                                   .replace(/[\t]/g, '\\t');
 
   console.log(strOpenApiData);
   
