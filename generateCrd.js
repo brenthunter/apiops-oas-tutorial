b484afe6b2e280693c0c9f3c7c41686f3768686c
Brent Hunter <brenthunter@users.noreply.github.com>
2025-05-07 09:18:45 +0100

    Update generateCrd.js

--- generateCrd.js
+++ generateCrd.js
@@ -99,7 +99,11 @@ function generateCRD(openApiData, propertiesData) {
     let yamlString = yaml.dump(openApiData, { lineWidth: -1 });
     // Step 2: Escape for use as a JavaScript string literal
     let escapedYamlStringLiteral = JSON.stringify(yamlString);
+    var NEWescapedYamlStringLiteral = "";
+    escapedYamlStringLiteral.split("\\n").forEach(line => NEWescapedYamlStringLiteral += "\\ " + line + " \\");
     resources[0].configuration.content = escapedYamlStringLiteral;
+    resources[0].configuration.contentNEW = NEWescapedYamlStringLiteral;
+    
     // Step 3: Update existing flows? with OpenAPI Spec Validator Policy configuration
     let flowsOpenApiSpec = {
       name: "OpenAPI Specification Validation",
